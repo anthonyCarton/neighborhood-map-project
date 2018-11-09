6593ef09244977f7af47e99edef88c21e684dca6
Anthony Carton <mr.carton@gmail.com>
2018-11-09 16:40:47 -0700

    fix: PropTypes update

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {GoogleApiComponent, GoogleAPIWrapper} from 'google-maps-react';
+import PropTypes from 'prop-types';
 
 
-class Map extends React.Component {
+
+export class Map extends React.Component {
 	componentDidUpdate(prevProps, prevState) {
-		if (prefProps.google !== this.props.google) {
+		if (prevProps.google !== this.props.google) {
 			this.loadMap();
 		}
+		if (prevState.currentLocation !== this.state.currentLocation) {
+			this.recenterMap();
+		}
 	}
+
+	recenterMap() {
+    const map = this.map;
+    const curr = this.state.currentLocation;
+
+    const google = this.props.google;
+    const maps = google.maps;
+
+    if (map) {
+        let center = new maps.LatLng(curr.lat, curr.lng)
+        map.panTo(center)
+    }
+  }
+
+	constructor(props) {
+    super(props);
+
+    const {lat, lng} = this.props.initialCenter;
+    this.state = {
+      currentLocation: {
+        lat: lat,
+        lng: lng
+      }
+    }
+  }
+
+	componentDidMount() {
+    if (this.props.centerAroundCurrentLocation) {
+        if (navigator && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                const coords = pos.coords;
+                this.setState({
+                    currentLocation: {
+                        lat: coords.latitude,
+                        lng: coords.longitude
+                    }
+                })
+            })
+        }
+    }
+    this.loadMap();
+  }
 
 	loadMap() {
 		if (this.props && this.props.google) {
@@ -19,10 +68,9 @@ class Map extends React.Component {
 			const node = ReactDOM.findDOMNode(mapRef)
 
 			// instantiate a Google map object on our page
-			let zoom = this.props.zoom;
-			let lat = this.props.lat;
-			let lng = this.props.lng;
-			const center = new.maps.LatLng(lat, lng);
+			let {initialCenter, zoom} = this.props;
+			const {lat, lng} = this.state.currentLocation;
+			const center = new maps.LatLng(lat, lng);
 			const mapConfig = Object.assign({}, {
 				center: center,
 				zoom: zoom
@@ -34,11 +82,27 @@ class Map extends React.Component {
 
 	render() {
     return (
-      <div ref='map'>
-        Loading map...
-      </div>
+			<div id="map" ref='map' role="application">
+				Loading map...
+				<p>{this.props.zoom}, {this.props.lat}, {this.props.lng}</p>
+			</div>
     )
   }
+}
+
+Map.propTypes = {
+  google: PropTypes.object,
+  zoom: PropTypes.number,
+  initialCenter: PropTypes.object
+}
+Map.defaultProps = {
+  zoom: 13,
+  // San Francisco, by default
+  initialCenter: {
+    lat: 37.774929,
+    lng: -122.419416
+  },
+	centerAroundCurrentLocation: false
 }
 
 export default Map

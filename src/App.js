a5d4f01b17422e96c6ab41a4c99bb306a1d30a1e
Anthony Carton <mr.carton@gmail.com>
2018-11-09 17:54:43 -0700

    refactor: Project restart / Second Initial Commit
    Fullstack React tutorial doesn't work,
    deleted components starting over.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
-import Container from './Container.js';
 import locations from './data/locations.json';
+import MapDisplay from './MapDisplay'
+
+
+const API_KEY = 'AIzaSyBlvE_2fUrpkkd2H0hMei-27nw_axyaVY0';
 
 class App extends Component {
 	// important to keep state at this higher level
 	state = {
-		lat: 111,
-		lng: 222,
+		lat: 37.774929,
+		lng: -122.419416,
 		zoom: 14,
-		locations: locations
+		locations: locations,
+		key: API_KEY
 	}
   render() {
     return (
       <div className="App">
-				<Container />
+				<h1>Mexican Restaurants in Mancos Colorado</h1>
+				<MapDisplay />
       </div>
     );
   }
---
+++ src/MapDisplay.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+// TODO: Loading the Google API
+// TODO: Handling access to the Google API within our components.
+
+class MapDisplay extends React.Component {
+  render() {
+    return (
+			<div>
+				<p>The map will be here</p>
+			</div>
+		);
+  }
+}
+
+export default MapDisplay

2338e85ca45019268616a5e6d3b5b749104c12b7
Anthony Carton <mr.carton@gmail.com>
2018-11-10 14:19:23 -0700

    docs: Remove phase 2 TODOs (completed)

--- src/App.js
+++ src/App.js
@@ -4,22 +4,6 @@ import './App.css';
 import locations from './data/locations.json';
 import MapDisplay from './MapDisplay';
 
-// TODOs: Phase 2 display map markers identifying at least 5 locations
-	// DONE: Add marker props to MapDisplay state (markers, markerProps, activeMarker, activeMarkerProps, showing in window)
-	// DONE: Create updateMarkers() method and call when mapReady
-		// DONE: make sure locations are valid
-		// DONE: Clear any existing markers
-		// DONE: Create an empty markerProps array
-		// DONE: Iterate through markers, for every location, get data, index and array
-		// DONE: push marker data into markerProps array
-	// DONE: Create onMarkerClick()
-		// DONE: Close any open info window
-		// DONE: Set state to marker info window show
-	// DONE: create closeInfoWindow
-		// DONE: Disable any active info window
-		// DONE: set state so no active windows
-	// DONE: Add InfoWindow object
-
 // TODOs: Phase 3 Implement a list view of the set of locations you have defined
 	// TODO: Add phase 3 TODOs
 // TODOs: Phase 4 Provide a filter option

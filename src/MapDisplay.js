58ef59f6a364eb17bcb5fcc8a65d559418f69085
Anthony Carton <mr.carton@gmail.com>
2018-11-09 18:50:02 -0700

    docs: Add TODOs after watching Doug Brown's walkthrough
    I am watching Mr. Brown's walkthrough, see README, as well as following
    the provided google-maps-react tutorial. The tutorial itself wasn't
    working for me, but Doug's walkthrough is helping it make more sense.
    I'm working to get the steps from him, do the work myself and then check
    based on his walkthrough.

--- src/MapDisplay.js
+++ src/MapDisplay.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import {Map, GoogleApiWrapper} from 'google-maps-react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
+// TODO: Add state with map: null
+// TODO: Add empty compDidMount()
+// TODO: Add mapReady(props, map)
+// TODO: Add style and center variables
+// TODO: Add ARIA role and label
+// TODO: Use GoogleApiWrapper to load the API, pass in MapDisplay when promise is fulfilled
+
 class MapDisplay extends React.Component {
   render() {
     return (

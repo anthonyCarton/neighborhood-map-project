9acbc14784df00c57e3bb903322851c9a245f6ad
Anthony Carton <mr.carton@gmail.com>
2018-11-09 19:07:58 -0700

    feat: Add style and center variables

--- src/MapDisplay.js
+++ src/MapDisplay.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 import {Map, GoogleApiWrapper} from 'google-maps-react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-// TODO: Add style and center variables
 // TODO: Add ARIA role and label
 // TODO: Use GoogleApiWrapper to load the API, pass in MapDisplay when promise is fulfilled
 
 class MapDisplay extends React.Component {
 	state = {
 		map:null
-	}
+	};
 	componentDidMount() {
 
 	}
@@ -19,12 +18,26 @@ class MapDisplay extends React.Component {
   	const service = new google.maps.places.PlacesService(map);
 	}
   render() {
+		const style = {
+			// Prefer VW and VH to %
+		  width: '100vw',
+		  height: '100vh'
+		}
+		const center = {
+			lat: this.props.lat,
+			lng: this.props.lng
+		}
     return (
 			<div>
 				<h1>Restaurants in Mancos Colorado</h1>
 				<p>The map will be here</p>
 				<Map
 					onReady={this.mapReady}
+					style={style}
+					// initalCenter: Takes an object containing latitude and longitude coordinates. Sets the maps center upon loading.
+					initialCenter={center}
+
+				/>
 			</div>
 		);
   }

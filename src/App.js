288049a87209d191de60613d02f7989e4c2bd0dd
Anthony Carton <mr.carton@gmail.com>
2018-11-09 18:07:50 -0700

    feat: Update project name

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-				<h1>Mexican Restaurants in Mancos Colorado</h1>
+				<h1>Restaurants in Mancos Colorado</h1>
 				<MapDisplay />
       </div>
     );

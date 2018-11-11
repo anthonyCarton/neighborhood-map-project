1ccaee7a341a08282a12bf51e2c1d6472279a35b
Anthony Carton <mr.carton@gmail.com>
2018-11-10 20:34:09 -0700

    feat: List of restaurants displaying
    List of restaurants display, with correct keys. React docs warn against
    using index unless absolutely necessary.
    
    https://reactjs.org/docs/lists-and-keys.html#extracting-components-with-keys

--- src/ListView.js
+++ src/ListView.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react'
 
 //https://reactjs.org/docs/lists-and-keys.html#embedding-map-in-jsx
 
-let numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) =>
-	<li>{number}</li>
-);
-
 class ListView extends React.Component {
+	state = {}
 
   render() {
     return (
 			<div className="list-view">
-				<p>This is the ListView</p>
-				<ul>{listItems}</ul>
+				<ul>
+					{this.props.locations.map((location) => {
+						return (
+						<li key={location.name}>
+							<button>{location.name}</button>
+						</li>
+					)
+					})}
+				</ul>
 			</div>
 		);
   }

0d7325e769f3532615586279bd071cbde617faaf
Anthony Carton <mr.carton@gmail.com>
2018-11-10 20:38:36 -0700

    feat: Implement a list view of the set of locations you have defined.
    Cleared TODOs for this as well

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import locations from './data/locations.json';
 import MapDisplay from './MapDisplay';
 import ListView from './ListView';
 
-// TODOs: Phase 4 Implement a list view of the set of locations you have defined
-	// TODO: Identify P4 TODOs
-	// TODO: Create a listdrawer component
-	// TODO: Map over locations and create list item > buttons
-	// TODO: CSS for the list, buttons, etc
-
 // TODOs: Phase 5 Provide a filter option
+	// TODO: Identify TODOs
+	
 // TODOs: Phase 6 Ensure responsive design
 
 class App extends Component {

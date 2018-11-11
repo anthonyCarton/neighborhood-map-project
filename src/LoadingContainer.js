eefb535bff3666d84105cb0c686ae64ccdf36a53
Anthony Carton <mr.carton@gmail.com>
2018-11-11 14:41:36 -0700

    feat: Add LoadingContainer
    per https://www.npmjs.com/package/google-maps-react#automatically-lazy-loading-google-api

---
+++ src/LoadingContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class LoadingContainer extends Component {
+	state = {}
+	componentDidMount = () => {}
+	componentWillUnmount = () => {}
+
+  render() {
+    return (
+			<div>
+				<h1></h1>
+				<p></p>
+			</div>
+		);
+  }
+}
+
+export default LoadingContainer
